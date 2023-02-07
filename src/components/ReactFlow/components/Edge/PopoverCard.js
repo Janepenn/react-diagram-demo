fda0725e51ad23e38c40c0e26783b734fed1d1ab
t-jianz <1532241119@qq.com>
2023-02-07 14:26:05 +0800

    feat: react flow commit

---
+++ src/components/ReactFlow/components/Edge/PopoverCard.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { FlowContext } from '../../context';
+
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
+export default function PopoverCard({ edge, source, target }) {
+  const {
+    state: { flowData }
+  } = useContext(FlowContext);
+
+  const sourceNode = flowData.get(source);
+  const targetNode = flowData.get(target);
+
+  return (
+    <div className="linkedge-card">
+      <div className="linkedge-card-item">
+        <div className="linkedge-card-item__title">{sourceNode?.label}</div>
+        <div className="linkedge-card-item__tips">{sourceNode?.remark}</div>
+      </div>
+      <div className="linkedge-card-item__icon"> == </div>
+      <div className="linkedge-card-item">
+        <div className="linkedge-card-item__title">{targetNode?.label}</div>
+        <div className="linkedge-card-item__tips">{targetNode?.remark}</div>
+      </div>
+    </div>
+  );
+}
--- src/components/ReactFlow/index.js
+++ src/components/ReactFlow/index.js
@@ -1,35 +1,29 @@
-import React, { useCallback } from 'react';
-import ReactFlow, { MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge } from 'reactflow';
-// import the reactflow styles
-import 'reactflow/dist/style.css';
+import React from 'react';
+import { ReactFlowProvider } from 'react-flow-renderer';
+import Sider from './Sider';
+import Graph from './Graph';
+import Modal from './components/Modal';
+// 引入 Provider
+import { FlowContextProvider } from './context';
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } }
-];
+import './flow.scss';
+import Toolbar from './Toolbar';
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-
-const Flow = () => {
-  // eslint-disable-next-line
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
-
+const FlowPage = () => {
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-    >
-      <MiniMap />
-      <Controls />
-      <Background />
-    </ReactFlow>
+    <div className="container">
+      <FlowContextProvider>
+        <ReactFlowProvider>
+          <Toolbar />
+          <div className="main">
+            <Sider />
+            <Graph />
+          </div>
+          <Modal />
+        </ReactFlowProvider>
+      </FlowContextProvider>
+    </div>
   );
 };
 
-export default Flow;
+export default FlowPage;
